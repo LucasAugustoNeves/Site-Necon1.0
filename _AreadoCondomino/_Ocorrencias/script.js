6cdc11a98486f826c3f76abe0f13100bc50d9b64
LucasAugustoNeves <lucasagneves@gmail.com>
2020-12-22 10:06:56 -0300

    painel condominio

---
+++ _AreadoCondomino/_Ocorrencias/script.js
@@ -0,0 +1,45 @@
+"use strict";
+
+$(document).ready(function domReady() {
+    $(".js-select2").select2({
+        placeholder: "Pick states",
+        theme: "material"
+    });
+    
+    $(".select2-selection__arrow")
+        .addClass("material-icons")
+        .html("arrow_drop_down");
+});
+
+//material contact form animation
+$('.contact-form').find('.form-control').each(function () {
+    var targetItem = $(this).parent();
+    if ($(this).val()) {
+      $(targetItem).find('label').css({
+        'top': '10px',
+        'fontSize': '14px'
+      });
+    }
+  })
+  $('.contact-form').find('.form-control').focus(function () {
+    $(this).parent('.input-block').addClass('focus');
+    $(this).parent().find('label').animate({
+      'top': '10px',
+      'fontSize': '14px'
+    }, 300);
+  })
+  $('.contact-form').find('.form-control').blur(function () {
+    if ($(this).val().length == 0) {
+      $(this).parent('.input-block').removeClass('focus');
+      $(this).parent().find('label').animate({
+        'top': '25px',
+        'fontSize': '18px'
+      }, 300);
+    }
+  })
+
+
+
+
+
+ 
